f61857ad1740922e61be68e6cabcb446c45bc57f
KikoEnakTau <theo.niomba@gmail.com>
2026-05-10 13:30:13 +0700

    fix: unloaded app

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,20 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
+    dedupe: [
+      '@noble/hashes',
+      '@noble/curves',
+      '@noble/ciphers',
+      'react',
+      'react-dom',
+    ],
+  },
+  optimizeDeps: {
+    include: [
+      '@noble/hashes/sha256',
+      '@noble/hashes/sha512',
+      '@noble/curves/secp256k1',
+      '@noble/curves/ed25519',
+    ],
   },
 })
